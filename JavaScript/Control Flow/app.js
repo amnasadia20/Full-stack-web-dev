f58c65948fffa96468910c9168a8d7109703bcd0
amnasadia20 <murtazaamna117@gmail.com>
2025-08-15 13:09:44 +0500

    4th

---
+++ JavaScript/Control Flow/app.js
@@ -0,0 +1,17 @@
+/*
+if you have a money,
+buy some stuff
+else don't have any money,
+Bring some money dude
+*/
+let age = 22;
+if (age == 18) {
+    console.log("You are not allowed to attend the party");
+    
+} else if(age == 21) {
+    console.log("You can attend the party.");
+    
+} else {
+    console.log("Go home bro");
+    
+}
