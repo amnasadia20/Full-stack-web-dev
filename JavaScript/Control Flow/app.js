fb229a6c9158c6e35d3d9069a1900c70346ec894
amnasadia20 <murtazaamna117@gmail.com>
2025-08-16 21:52:10 +0500

    1st

--- JavaScript/Control Flow/app.js
+++ JavaScript/Control Flow/app.js
@@ -15,3 +15,16 @@ if (age == 18) {
     console.log("Go home bro");
     
 }
+
+let a = 100;
+let b = 100;
+
+if(a>b) {
+    console.log("a is greater than b")
+} else if(a === b) {
+    console.log("a is equal to b");
+    
+} else {
+    console.log("a is less than b");
+    
+};
